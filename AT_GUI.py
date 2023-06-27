f5381b5dbbdb06fc41793b312e2a0e51843f5d74
vishnuocv <vishnuocv@gmail.com>
2023-06-27 19:08:50 +0900

    more design changes

--- AT_GUI.py
+++ AT_GUI.py
@@ -21,46 +21,46 @@ window.title("APN Setter GUI")
 # Adjusting the window size and position
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
-window_width = 700
-window_height = 500
+window_width = 650
+window_height = 600
 window_x = (screen_width - window_width) // 2
 window_y = (screen_height - window_height) // 2
 window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 # Create the APN entry field
-apn_lbl = tk.Label(text='Access point name')
+apn_lbl = tk.Label(text='Access point name', font=('Arial', 12))
 apn_lbl.pack()
 apn_entry = tk.Entry(window, width=50, font=('Arial', 12))
-apn_entry.pack(pady=5)
+apn_entry.pack(pady=6)
 
 # Create the IP type entry field
-ip_lbl = tk.Label(text='IP TYPE (IPV4, IPV6 or IPV4V6)')
+ip_lbl = tk.Label(text='IP TYPE (IPV4, IPV6 or IPV4V6)', font=('Arial', 12))
 ip_lbl.pack()
 ip_entry = tk.Entry(window, width=50, font=('Arial', 12))
-ip_entry.pack(pady=5)
+ip_entry.pack(pady=6)
 
 # Create the Authentication entry field
-auth_lbl = tk.Label(text='Authentication (CHAP, PAP or CHAP/PAP)')
+auth_lbl = tk.Label(text='Authentication (CHAP, PAP or CHAP/PAP)', font=('Arial', 12))
 auth_lbl.pack()
 auth_entry = tk.Entry(window, width=50, font=('Arial', 12))
-auth_entry.pack(pady=5)
+auth_entry.pack(pady=6)
 
 # Create the User name entry field
-user_lbl = tk.Label(text='User name')
+user_lbl = tk.Label(text='User name', font=('Arial', 12))
 user_lbl.pack()
 user_entry = tk.Entry(window, width=50, font=('Arial', 12))
-user_entry.pack(pady=5)
+user_entry.pack(pady=6)
 
 # Create the Password entry field
-pass_lbl = tk.Label(text='Password')
+pass_lbl = tk.Label(text='Password', font=('Arial', 12))
 pass_lbl.pack()
 pass_entry = tk.Entry(window, width=50, font=('Arial', 12))
-pass_entry.pack(pady=5)
+pass_entry.pack(pady=6)
 
-button = tk.Button(window, text="Set APN", width=20, bg='#ffffff', activebackground='#00ff00', command=send_at_command)
-button.pack(pady=10)
+button = tk.Button(window, text="Set APN", font=('Arial', 12), width=20, bg='#ffffff', activebackground='#00ff00', command=send_at_command)
+button.pack(pady=15)
 
-exit_button = tk.Button(window, text = "Quit", width=20, bg='#ffffff', activebackground='red',
+exit_button = tk.Button(window, text = "Quit", font=('Arial', 12), width=20, bg='#ffffff', activebackground='red',
             command = window.destroy)
 exit_button.pack(pady=15)
 
