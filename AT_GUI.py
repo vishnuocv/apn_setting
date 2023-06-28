cc132333bf53eec31fccb081ab4a5820ba1cfc86
vishnuocv <vishnuocv@gmail.com>
2023-06-28 10:32:54 +0900

    Check for MMCLI port and select the port accordingly

--- AT_GUI.py
+++ AT_GUI.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 import subprocess
 
+def mmcli_port_checker():
+    mmcli_port_check = subprocess.check_output(['mmcli', '-L']).decode('utf-8')
+    mmcli_port = mmcli_port_check.rfind("/")
+    mmcli_port_number = mmcli_port_check[mmcli_port + 1:].strip()
+    return mmcli_port_number	
+
 def send_at_command():
     apn_name = apn_entry.get()
     ip_type = ip_entry.get()
@@ -8,11 +14,11 @@ def send_at_command():
     user_name = user_entry.get()
     password = pass_entry.get()
     command = '--3gpp-set-initial-eps-bearer-settings=apn=' + apn_name + ',ip-type=' + ip_type + ',allowed-auth=' + auth_type + ',user=' + user_name + ',password=' + password
-    print (command)
-    result = subprocess.run(['mmcli', '-m', '0', command], capture_output=True, text=True)
+    port_number = mmcli_port_checker()
     output_text.delete('1.0', tk.END)  # Clear previous output
+    output_text.insert(tk.END, "Port detected is " + port_number + '\n')
+    result = subprocess.run(['mmcli', '-m', port_number, command], capture_output=True, text=True)
     output_text.insert(tk.END, result.stdout)
-    output_text.insert(tk.END, command)
     output_text.insert(tk.END, result.stderr)
 
 window = tk.Tk()
@@ -46,13 +52,13 @@ auth_entry = tk.Entry(window, width=50, font=('Arial', 12))
 auth_entry.pack(pady=6)
 
 # Create the User name entry field
-user_lbl = tk.Label(text='User name', font=('Arial', 12))
+user_lbl = tk.Label(text='User name  (if any)', font=('Arial', 12))
 user_lbl.pack()
 user_entry = tk.Entry(window, width=50, font=('Arial', 12))
 user_entry.pack(pady=6)
 
 # Create the Password entry field
-pass_lbl = tk.Label(text='Password', font=('Arial', 12))
+pass_lbl = tk.Label(text='Password (if any)', font=('Arial', 12))
 pass_lbl.pack()
 pass_entry = tk.Entry(window, width=50, font=('Arial', 12))
 pass_entry.pack(pady=6)
@@ -72,4 +78,3 @@ output_text.pack(pady=15)
 output_text.insert(tk.END, "Please Enter APN details in the text box")
 
 window.mainloop()
-
