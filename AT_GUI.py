ffdc4f3f6126b7fce13a1b2c3213563c8b22dac7
vishnuocv <vishnuocv@gmail.com>
2023-06-30 20:30:21 +0900

    Added chages o automate nm-connection-editor, so users just need to enter in the APN Setter UI only

--- AT_GUI.py
+++ AT_GUI.py
@@ -1,6 +1,65 @@
 import tkinter as tk
 import subprocess
 
+def create_or_modify_mobile_broadband_profile(profile_name, apn, ip_type, username, password, authentication):
+    # Check if the profile already exists
+    result = subprocess.run(["nmcli", "-t", "-f", "NAME", "c", "show"], capture_output=True, text=True)
+    existing_profiles = result.stdout.splitlines()
+    if profile_name in existing_profiles:
+        # Modify the existing profile
+        subprocess.run(["nmcli", "c", "modify", profile_name, "gsm.apn", apn])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "gsm.username", username])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "gsm.password", password])
+        print("Mobile broadband profile modified successfully.")
+    else:
+        # Create a new profile
+        subprocess.run(["nmcli", "c", "add", "type", "gsm", "ifname", "none", "con-name", profile_name])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "gsm.apn", apn])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "gsm.username", username])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "gsm.password", password])
+        print("Mobile broadband profile created successfully.")
+
+    # Configure authentication options based on the selected method
+    if authentication.lower() in ["pap"]:
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-eap", "yes"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-chap", "yes"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-mschap", "yes"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-mschapv2", "yes"])
+    elif authentication.lower() in ["chap"]:
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-eap", "yes"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-pap", "yes"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-mschap", "yes"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-mschapv2", "yes"])
+    elif authentication.lower() in ["pap/chap", "chap/pap"]:
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-eap", "yes"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-chap", "no"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-pap", "no"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-mschap", "yes"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ppp.refuse-mschapv2", "yes"])
+
+    # Configure IP type options
+    if ip_type.lower() == "ipv4":
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ipv4.method", "auto"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ipv6.method", "disabled"])
+    elif ip_type.lower() == "ipv6":
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ipv4.method", "disabled"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ipv6.method", "auto"])
+    elif ip_type.lower() == "ipv4v6":
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ipv4.method", "auto"])
+        subprocess.run(["nmcli", "c", "modify", profile_name, "ipv6.method", "auto"])
+
+
+    # Bring up the mobile broadband connection
+    result = subprocess.run(["nmcli", "con", "up", profile_name])
+    if result==0:
+        print("Mobile broadband connection is up.")
+    else: 
+        print("Mobile broadband connection is not up. Either modem is not detected or driver is not up")
+
+    output_text.insert(tk.END, result.stdout)
+    output_text.insert(tk.END, result.stderr)
+
+
 def mmcli_port_checker():
     mmcli_port_check = subprocess.check_output(['mmcli', '-L']).decode('utf-8')
     mmcli_port = mmcli_port_check.rfind("/")
@@ -9,18 +68,29 @@ def mmcli_port_checker():
     return port[0]	
 
 def send_at_command():
+    #filling variables from textbox
+    profile_name = profile_entry.get()
     apn_name = apn_entry.get()
     ip_type = ip_entry.get()
     auth_type = auth_entry.get()
     user_name = user_entry.get()
     password = pass_entry.get()
+
+    #fixing the commands
     command = '--3gpp-set-initial-eps-bearer-settings=apn=' + apn_name + ',ip-type=' + ip_type + ',allowed-auth=' + auth_type + ',user=' + user_name + ',password=' + password
+
+    #calling port checker function
     port_number = mmcli_port_checker()
     output_text.delete('1.0', tk.END)  # Clear previous output
     output_text.insert(tk.END, "Port detected is " + port_number + '\n')
     result = subprocess.run(['mmcli', '-m', port_number, command], capture_output=True, text=True)
     output_text.insert(tk.END, result.stdout)
     output_text.insert(tk.END, result.stderr)
+
+    #setting values in Network manager and calling connect
+    create_or_modify_mobile_broadband_profile(profile_name, apn_name, ip_type, user_name, password, auth_type)
+
+
 
 window = tk.Tk()
 window.title("APN Setter GUI")
@@ -29,10 +99,16 @@ window.title("APN Setter GUI")
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 window_width = 650
-window_height = 600
+window_height = 700
 window_x = (screen_width - window_width) // 2
 window_y = (screen_height - window_height) // 2
 window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+# Create the APN entry field
+profile_lbl = tk.Label(text='Profile Name', font=('Arial', 12))
+profile_lbl.pack()
+profile_entry = tk.Entry(window, width=50, font=('Arial', 12))
+profile_entry.pack(pady=6)
 
 # Create the APN entry field
 apn_lbl = tk.Label(text='Access point name', font=('Arial', 12))
@@ -64,7 +140,8 @@ pass_lbl.pack()
 pass_entry = tk.Entry(window, width=50, font=('Arial', 12))
 pass_entry.pack(pady=6)
 
-button = tk.Button(window, text="Set APN", font=('Arial', 12), width=20, bg='#ffffff', activebackground='#00ff00', command=send_at_command)
+
+button = tk.Button(window, text="Set APN and Connect", font=('Arial', 12), width=20, bg='#ffffff', activebackground='#00ff00', command=send_at_command)
 button.pack(pady=15)
 
 exit_button = tk.Button(window, text = "Quit", font=('Arial', 12), width=20, bg='#ffffff', activebackground='red',
